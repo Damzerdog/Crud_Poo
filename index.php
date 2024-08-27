216bcb0775106f77a3635f5c5a98d7a6a51f3c3a
Izmad D. Uchiha <danpe808@gmail.com>
2024-08-27 08:50:58 -0600

    Inicio curso CRUD_POO

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+/** Crud
+ * Create
+ * Read
+ * Update
+ * Delete
+*/
+/**MVC
+ * Es un patron de arquitectura de software que separa en 3 partes distintas los componentes una aplicacion.
+ * {Modelo} Es la parte que se encarga de interactuar directamente con la base de datos.
+ * {Vista} Es la parte donde se encuentra ña interfaz grafica de la aplicacion con la cual el usuario interactua.
+ * {Controlador} Es la que parte que interactua como intermediario entre el modelo y la vista. {vista->modelo; modelo->controlador y de regreso}
+ */
+/** .HTACCESS {HyperText Access}
+ * Es un archivo de configuración del servidor Apache, que contiene las directivas que definen el comportamiento Apache.
+ * Indica en todo momento qué puede hacer y qué no el usuario que visita tu web, así como configurar el comportamiento del servidor ante errores de conexión.
+ * Puedes reescribir la URL, proteger directorios con contraseñas, habilitar la protección de enlaces directos, no permitir el acceso a direcciones IP especificas, cambiar la zona horaria de tu sitio web o alterar la página de índice predeterminada.
+ */
